52113da974c0f0c3d2002b4209f27daf3f43aa87
Christoph Mewes <github@xrstf.de>
2023-05-23 14:47:07 +0200

    Update Prow (#4)
    * bump prow
    
    * fix script to create initial configmaps

--- clusters/prow/hack/create-prow-configmaps.sh
+++ clusters/prow/hack/create-prow-configmaps.sh
@@ -10,20 +10,22 @@ cd $(dirname $0)/..
 source hack/settings.sh
 source hack/lib.sh
 
+PROW_DIR="$(realpath ../../prow)"
+
 # configure prow itself
 kubectl --namespace "$PROW_NAMESPACE" create configmap config \
-  --from-file=config.yaml=prow/config.yaml \
+  --from-file="config.yaml=$PROW_DIR/config.yaml" \
   --dry-run=client \
   --output=yaml \
   | kubectl apply -f -
 
 # ... and its plugins
 kubectl --namespace "$PROW_NAMESPACE" create configmap plugins \
-  --from-file=plugins.yaml=prow/plugins.yaml \
+  --from-file="plugins.yaml=$PROW_DIR/plugins.yaml" \
   --dry-run=client \
   --output=yaml \
   | kubectl apply -f -
 
 # ... and its global jobs
-create_job_config_configmap
+create_job_config_configmap "$PROW_DIR"
 kubectl replace -f job-config.yaml
