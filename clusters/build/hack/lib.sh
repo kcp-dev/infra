21aed6a50a23d2ceee332f4668531ab3357304ba
Marvin Beckers <marvin@kubermatic.com>
2025-06-10 17:24:23 +0200

    Add echodate to lib.sh
    On-behalf-of: SAP <marvin.beckers@sap.com>
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- clusters/build/hack/lib.sh
+++ clusters/build/hack/lib.sh
@@ -2,3 +2,7 @@ sed_settings() {
   # $1 is purposefully not quoted here to allow globbing
   sed --in-place "s;__PROW_TESTPOD_NAMESPACE__;$PROW_TESTPOD_NAMESPACE;g" $1
 }
+
+echodate() {
+  echo "[$(date +%Y-%m-%dT%H:%M:%S%:z)]" "$@"
+}
